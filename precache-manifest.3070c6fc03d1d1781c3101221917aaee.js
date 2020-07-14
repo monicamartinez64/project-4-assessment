6d06bb19054414ac05cee1ea182b5093d4546c15
monicamartinez64 <monicamartinezmj@gmail.com>
2020-07-14 14:26:00 -0500

    Updates

---
+++ precache-manifest.3070c6fc03d1d1781c3101221917aaee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e7e77d9682a0caea4a81bb5313176b",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "800a7b156739e2273c23",
+    "url": "/project-4-assessment/static/css/main.d5eabd75.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800a7b156739e2273c23",
+    "url": "/project-4-assessment/static/js/main.b5bf9d66.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
